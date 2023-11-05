ecf57190f333f3fa1f66341a6d539b030e958cc1
Md Abdullah <shamsabdullah98@gmail.com>
2023-11-05 08:56:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const uuid = require('uuid');
+
+
+const defaultRoutes = require('./routes/default');
+const resaurantRoutes = require('./routes/restaurants');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', defaultRoutes);
+app.use('/', resaurantRoutes);
+
+
+
+app.use(function(req, res) {
+    res.status(404).render('404');
+});
+
+app.use(function(error, req, res, next) {
+    res.status(500).render('500');
+})
+
+app.listen(3000);
